3606c29cf4ba8dc73cb4ab1e56f7ec9452aa9fbd
retcon1 <benjamin-lucas@hotmail.co.uk>
2023-04-05 17:01:19 +0100

    gets further? in click attack

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -5,47 +5,57 @@ const Button = () => {
   const [seconds, setSeconds] = useState(0);
   const [isRunning, setIsRunning] = useState(false);
   const [hasStarted, setHasStarted] = useState(false);
-  
-  useEffect(() => {
-    if(isRunning) {
-      const interval = setInterval(() => {
-        if(seconds >= 10) {
-          clearInterval(interval);
-          console.log('hello');
-          setIsRunning(false);
-          setHasStarted(false);
-          return () => setSeconds(10);
-        }
-        setSeconds(seconds => seconds + 1);
-      }, 1000);
-    }
-      
-    // return () => 
 
-  }, [isRunning]);
-  
-  
-  
-  
-  
+  useEffect(() => {
+    let secs = 0;
+    if (isRunning) {
+      const interval = setInterval(() => {
+        secs += 1;
+        setSeconds(seconds + 1)
+      }, 1000);
+
+      if (seconds === 10) {
+        setHasStarted(false);
+        setIsRunning(false);
+        return clearInterval(interval);
+      }
+    }
+  }, [isRunning, seconds]);
+
+  // if(seconds >= 10) {
+  //   clearInterval(interval);
+  //   console.log('hello');
+  //   setIsRunning(false);
+  //   setHasStarted(false);
+  //   return () => setSeconds(10);
+  // }
+  // setSeconds(seconds => seconds + 1);
+  // return () =>
+
   return (
     <div>
       <h2>Seconds: {seconds}</h2>
-      {!isRunning ? <button
-        onClick={(event) => {
-          if(!hasStarted) {
-            setHasStarted(true);
-            setIsRunning(true);
-          }
-    
-         
-        }}
-      >Click to start</button> : <button onClick={() => {
-        setScore(score + 1);
-     }}>
-       Click Me!
-     </button>}
-      
+      {!isRunning ? (
+        <button
+          onClick={(event) => {
+            if (!hasStarted) {
+              setHasStarted(true);
+              setIsRunning(true);
+            }
+          }}
+        >
+          Click to start
+        </button>
+      ) : (
+        <button
+          onClick={() => {
+            setScore(score + 1);
+          }}
+        >
+          Click Me!
+        </button>
+      )}
+
       <h2>Score: {score}</h2>
     </div>
   );
